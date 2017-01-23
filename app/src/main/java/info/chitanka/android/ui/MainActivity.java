5c99467ef952e11f9982b3a22a87a624c04d6d79
Georgi Mirchev <gmirchev90@gmail.com>
2017-01-23 15:48:10 +0200

    New books and textbooks fragment. Control the main app bar shadow.

--- app/src/main/java/info/chitanka/android/ui/MainActivity.java
+++ app/src/main/java/info/chitanka/android/ui/MainActivity.java
@@ -3,6 +3,7 @@ package info.chitanka.android.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -45,10 +46,13 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Pres
     @Bind(R.id.nav_view)
     NavigationView navigationView;
 
+    @Bind(R.id.app_bar)
+    AppBarLayout appBarLayout;
+
     @Inject
     AnalyticsService analyticsService;
 
-    private int selectedNavItemId;
+    private int selectedNavItemId = R.id.nav_new;
 
     private NetworkRequiredDialog networkRequiredDialog;
     private PresenterComponent presenterComponent;
@@ -83,12 +87,11 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Pres
 
         if (savedInstanceState != null && savedInstanceState.containsKey(KEY_SELECTED_ITEM)) {
             selectedNavItemId = savedInstanceState.getInt(KEY_SELECTED_ITEM);
-            MenuItem item = navigationView.getMenu().findItem(selectedNavItemId);
-            onNavigationItemSelected(item);
-        } else {
-            navigationView.getMenu().getItem(0).setChecked(true);
-            onNavigationItemSelected(navigationView.getMenu().getItem(0));
         }
+
+        MenuItem item = navigationView.getMenu().findItem(selectedNavItemId);
+        item.setChecked(true);
+        onNavigationItemSelected(item);
     }
 
     @Override
@@ -177,7 +180,6 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Pres
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
 
-
         return true;
     }
 
@@ -198,12 +200,14 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Pres
             return;
         } else if (id == R.id.nav_email) {
             analyticsService.logEvent(TrackingConstants.CLICK_WRITE_US);
-            final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+            final Intent emailIntent = new Intent(Intent.ACTION_SEND);
             emailIntent.setType("plain/text");
-            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"contact@chitanka.info"});
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"contact@chitanka.info"});
             startActivity(Intent.createChooser(emailIntent, "Изпрати имейл"));
             return;
         }
+
+        updateAppBarShadow();
 
         if (fragment == null) {
             return;
@@ -215,6 +219,15 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Pres
         }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment, fragment.getTitle()).commit();
+    }
+
+    private void updateAppBarShadow() {
+        if (selectedNavItemId == R.id.nav_new) {
+            appBarLayout.setEnabled(false);
+        } else {
+            appBarLayout.setEnabled(true);
+        }
+        appBarLayout.requestLayout();
     }
 
     @Override
--- app/src/main/java/info/chitanka/android/ui/fragments/newest/NewBooksAndTextworksFragment.java
+++ app/src/main/java/info/chitanka/android/ui/fragments/newest/NewBooksAndTextworksFragment.java
@@ -2,6 +2,12 @@ package info.chitanka.android.ui.fragments.newest;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +16,19 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import info.chitanka.android.R;
+import info.chitanka.android.components.AnalyticsService;
 import info.chitanka.android.di.presenters.PresenterComponent;
 import info.chitanka.android.mvp.models.Book;
 import info.chitanka.android.mvp.models.TextWork;
 import info.chitanka.android.mvp.presenters.newest.NewBooksAndTextWorksPresenter;
 import info.chitanka.android.mvp.views.NewBooksAndTextWorksView;
+import info.chitanka.android.ui.fragments.AuthorsFragment;
 import info.chitanka.android.ui.fragments.BaseFragment;
+import info.chitanka.android.ui.fragments.TextWorksFragment;
+import info.chitanka.android.ui.fragments.books.BooksFragment;
 
 /**
  * Created by joro on 23.01.17.
@@ -28,6 +39,18 @@ public class NewBooksAndTextworksFragment extends BaseFragment implements NewBoo
 
     @Inject
     NewBooksAndTextWorksPresenter presenter;
+
+    @Inject
+    AnalyticsService analyticsService;
+
+    @Bind(R.id.container)
+    ViewPager mViewPager;
+
+    @Bind(R.id.tabs)
+    TabLayout tabLayout;
+
+    @Bind(R.id.app_bar)
+    AppBarLayout appBar;
 
     public static NewBooksAndTextworksFragment newInstance() {
         return new NewBooksAndTextworksFragment();
@@ -46,6 +69,13 @@ public class NewBooksAndTextworksFragment extends BaseFragment implements NewBoo
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_books_textworks, container, false);
         ButterKnife.bind(this, view);
+
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(mSectionsPagerAdapter.getCount());
+
+        tabLayout.setupWithViewPager(mViewPager);
+
         return view;
     }
 
@@ -79,5 +109,43 @@ public class NewBooksAndTextworksFragment extends BaseFragment implements NewBoo
     @Override
     public void presentNewBooksAndTextWorks(List<Book> books, List<TextWork> textWorks) {
 
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return BooksFragment.newInstance("");
+                case 1:
+                    return AuthorsFragment.newInstance("");
+                case 2:
+                    return TextWorksFragment.newInstance("", null);
+            }
+            return BooksFragment.newInstance("");
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_books);
+                case 1:
+                    return getString(R.string.title_authors);
+                case 2:
+                    return getString(R.string.title_textworks);
+            }
+            return null;
+        }
     }
 }
